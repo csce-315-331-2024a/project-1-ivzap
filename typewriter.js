9efe8adba1e8bd2b1df7486cb04211af9891867b
= <zaplatarivan@gmail.com>
2024-02-02 11:02:41 -0600

    removed bubbles

--- typewriter.js
+++ typewriter.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", async function () {
   var preBlocks = document.querySelectorAll(".terminal-text");
-
   async function typeText(preBlock, originalText, speed) {
-    var chunkSize = originalText.length > 50 ? 10 : 1;
+    var chunkSize = originalText.length > 50 ? 10 : 1; // make faster depending on character count
 
     for (var index = 0; index < originalText.length; index += chunkSize) {
       var chunk = originalText.substring(index, index + chunkSize);
       preBlock.textContent += chunk;
-      preBlock.style.visibility = "visible";
-      await new Promise((resolve) => setTimeout(resolve, speed)); // Delay for the typing effect
+      preBlock.style.visibility = "visible"; // let user be able to see the typing
+      await new Promise((resolve) => setTimeout(resolve, speed)); // delay for the typing effect
     }
   }
 
+  // for each element, start typing it out, writing to screen.
   for (const preBlock of preBlocks) {
+    // if stylesheet is style2 we dont want typing effect
     curStyleSheet = localStorage.getItem("savedStyleName");
     if (curStyleSheet == "style2.css") {
       break;
@@ -22,14 +23,14 @@ document.addEventListener("DOMContentLoaded", async function () {
       continue;
     }
     var originalText = preBlock.textContent;
-    preBlock.textContent = ""; // Clear the original text
+    preBlock.textContent = ""; // clear the original text
 
-    var speed = 10; // Adjust the speed value to make it faster
+    var speed = 10; // adjust the speed value to make it faster
 
     if (preBlock.tagName !== "PRE" && preBlock.classList.contains("fastType")) {
-      speed = 5; // Adjust the speed value for specific conditions
+      speed = 5; // adjust the speed value for specific conditions
     }
-
+    // perform typing effect
     await typeText(preBlock, originalText, speed);
   }
 });
